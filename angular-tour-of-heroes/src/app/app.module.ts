af9f6e6432933a07e1cbeb79e1463f4c396d1c19
TheGiver <faysalkharraz@gmail.com>
2021-12-13 10:17:49 +0200

    chore: Added a nice looking carousel for the heroes to be displayed in. Did some basic restructuring to the project to improve the overall design

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { CarouselModule } from 'ngx-carousel-lib';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +16,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   imports: [
     BrowserModule,
     FormsModule,
-    SlickCarouselModule,
+    CarouselModule,
     AppRoutingModule
   ],
   providers: [],
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,40 +1,16 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation} from '@angular/core';
 import { HEROES } from '../data_source/heroes/mock-heroes';
 
 @Component({
     selector: 'app-heroes',
     templateUrl: './heroes.component.html',
-    styleUrls: ['./heroes.component.css']
+    styleUrls: ['./heroes.component.css'],
+    encapsulation: ViewEncapsulation.Emulated
 })
 
 export class HeroesComponent implements OnInit {
     heroes = HEROES;
-    numberOfHeroes = this.heroes;
-    numberOfSlides = this.numberOfHeroes;
-
-    slideConfig={
-        "slidesToShow": this.numberOfHeroes,
-        "slidesToScroll": this.numberOfSlides,
-        "dots": true,
-        "infinite": false
-    };
-
-    slickInit(e: any) {
-        console.log('msg: slick carousel | extra details: ' + e);
-    }
-
-    breakpoint(e: any) {
-        console.log('msg: breakpoint | extra details: ' + e);
-    }
-
-    afterChange(e: any) {
-        console.log("msg: afterChange | extra details: " + e);
-    }
-
-    beforeChange(e: any) {
-        console.log("msg: beforeChange | extra details: " + e);
-    }
-
+    
     constructor() {}
 
     ngOnInit(): void {}
